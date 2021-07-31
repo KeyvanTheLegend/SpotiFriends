22e6da070a8129ec6072aa4cfba179529485067b
keyvanyaghoubian <keyvan.ygh@gmail.com>
2021-07-31 21:22:43 +0430

    init project

---
+++ SpotiFriends/SpotiFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotiFriendsApp.swift
+//  SpotiFriends
+//
+//  Created by sh on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SpotiFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
