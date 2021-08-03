c146865651d8c83b09d517053ddcd08c28b739d6
keyvanyaghoubian <keyvan.ygh@gmail.com>
2021-08-03 12:59:42 +0430

    added GradientBackground to Color extension

--- SpotiFriends/Color+extension.swift
+++ SpotiFriends/Color+extension.swift
@@ -11,5 +11,15 @@ import SwiftUI
 extension Color {
     static let BackgroundColor = Color("BackgroundColor")
     static let LightGray = Color("LightGray")
+    
+    static let GradientBackground = RadialGradient(
+        gradient: Gradient(
+            colors: [
+                Color.BackgroundColor.opacity(0.93),
+                Color.BackgroundColor]
+        ),
+        center: .center,
+        startRadius: 1,
+        endRadius: 500)
 
 }
